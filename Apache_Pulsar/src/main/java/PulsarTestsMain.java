63ee179749c3facf9fe2026e0ae52c1057916fa8
AndreyTokmakov <andtokm@yandex.ru>
2024-08-28 20:40:21 +0400

    28_08_2024__005

---
+++ Apache_Pulsar/src/main/java/PulsarTestsMain.java
@@ -0,0 +1,18 @@
+/****************************************************************************
+* Copyright 2021 (C) Andrey Tokmakov
+* AdminKafkaTests.java class
+*
+* @name    : AdminKafkaTests.java
+* @author  : Tokmakov Andrey
+* @version : 1.0
+* @since   : Feb 13, 2021
+****************************************************************************/
+
+
+public class PulsarTestsMain 
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Apache_Pulsar");
+	}
+}
