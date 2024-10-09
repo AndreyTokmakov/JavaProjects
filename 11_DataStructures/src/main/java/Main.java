2715194fb7d3bd2081a98ebb9e014e5125496aa4
Andrei.Tokmakov <andtokm@yandex.ru>
2024-10-09 22:25:02 +0400

    09_10_2024_002

---
+++ 11_DataStructures/src/main/java/Main.java
@@ -0,0 +1,19 @@
+public class Main
+{
+
+    public static String getString(int val)
+    {
+        switch (val) {
+            case 1 -> "Toilet";
+            case 2 -> "Bedroom";
+            case 30 -> "Classroom";
+            default -> "Unsupported value: " + reverb.roomSize();
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        int var = 10;
+
+    }
+}
