b72dafcdbb7455f4ade36295f55bf4428780ba96
Andrei.Tokmakov <andtokm@yandex.ru>
2024-12-06 22:22:49 +0400

    06_12_2024_003

--- 3_MULTITHREADING_TESTS/src/main/java/Virtual_Threads/VirtualThreads.java
+++ 3_MULTITHREADING_TESTS/src/main/java/Virtual_Threads/VirtualThreads.java
@@ -14,7 +14,7 @@ public class VirtualThreads
         }
     }
 
-    public static void main(String[] args) throws InterruptedException
+    protected static void simpleTest() throws InterruptedException
     {
         Thread thread1 = Thread.ofVirtual().start(() -> {
             System.out.println("Thread 1 started   at " + LocalDateTime.now());
@@ -30,6 +30,25 @@ public class VirtualThreads
 
         thread1.join();
         thread2.join();
+    }
 
+    protected static void builderTest() throws InterruptedException
+    {
+        Thread.Builder builder = Thread.ofVirtual().name("MyThread");
+        Runnable task = () -> {
+            System.out.println("Thread 1 started   at " + LocalDateTime.now());
+            sleepMs(500);
+            System.out.println("Thread 1 completed at " + LocalDateTime.now());
+        };
+        Thread t = builder.start(task);
+        System.out.println("Thread t name: " + t.getName());
+        t.join();
+    }
+
+    /// https://docs.oracle.com/en/java/javase/21/core/virtual-threads.html#GUID-2DDA5807-5BD5-4ABC-B62A-A1230F0566E0
+    public static void main(String[] args) throws InterruptedException
+    {
+        // simpleTest();
+        builderTest();
     }
 }
